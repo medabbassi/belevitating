fabd2623804dd520b4afc2f9ae49774074772e27
Mohamed Abbassi <mabbassix3@gmail.com>
2021-07-16 20:41:55 +0200

    fixing probems 0.4

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,17 @@ import 'package:belevitating/screens/character_list.dart';
 import 'package:flutter/material.dart';
 
 void main() {
- runApp(MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
- @override
- Widget build(BuildContext context) {
-  return MaterialApp(
-   debugShowCheckedModeBanner: false,
-   title: "B-elevetating",
-   theme: ThemeData(primarySwatch: Colors.blueGrey),
-   home: CharacterList(),
-  );
- }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "B-elevetating",
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: CharacterList(),
+    );
+  }
 }
-
--- lib/screens/character_list.dart
+++ lib/screens/character_list.dart
@@ -4,6 +4,7 @@ import 'package:belevitating/module/qoute.dart';
 import 'package:flutter/material.dart';
 import 'package:belevitating/data/quote_api.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:share/share.dart';
 
 class CharacterList extends StatefulWidget {
   CharacterList({Key? key}) : super(key: key);
@@ -24,6 +25,20 @@ class _CharacterListState extends State<CharacterList> {
     });
     print(characterList.length.toString());
   }
+  void _shareFuntion(String value1){
+    Share.share(value1);
+  }
+  bool _isLiked = true;
+  void _toogleLike(){
+    setState(() {
+      if(_isLiked){
+        _isLiked = false;
+      }
+      else {
+        _isLiked =true;
+      }
+    });
+  }
 
   @override
   void initState() {
@@ -36,48 +51,112 @@ class _CharacterListState extends State<CharacterList> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.lightBlueAccent,
-        body: Padding(
-          padding: EdgeInsets.all(8),
-          child: Container(
-            margin: EdgeInsets.all(8.0),
-            child: ListView.builder(
-                padding: EdgeInsets.all(5.0),
-                itemCount: characterList.length,
-                itemBuilder: (context, index) {
-                  return Card(
+        body: Stack(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height * .45,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(220, 220, 220, 0),
+              ),
+            ),
+            SafeArea(
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    children: [
+                      topBar(),
+                      SizedBox(height: 10.0),
+                      characterList.length>0 ?
+                      Expanded(
+                          child:ListView.builder(
+                          padding: EdgeInsets.all(5.0),
+                          itemCount: characterList.length,
+                          itemBuilder: (context, index) {
+                            return SizedBox(
+                              height: 550,
+                              child: Card(
+                                borderOnForeground: true,
+                                child: Container(
+                                  padding: EdgeInsets.all(10),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Align(
+                                        alignment: Alignment.topRight,
+                                        child: IconButton(
+                                          onPressed: _toogleLike,
+                                          icon:(_isLiked ?
+                                          Icon(Icons.favorite_border_outlined ):
+                                          Icon(Icons.favorite,color: Colors.pinkAccent,)),
+                                            //Icons.favorite_border_outlined
+                                        ),
+                                      ),
+                                      Text("\" " + characterList[index].text +" \"" ,
+                                          softWrap: true,
+                                          textAlign: TextAlign.center,
+                                          style: GoogleFonts.robotoCondensed(
+                                              fontSize: 50,
+                                              fontStyle: FontStyle.italic,
+                                              fontWeight: FontWeight.bold)),
 
-                    child: Column(
-                      children: [
-                        Align(
-                          alignment: Alignment.topRight,
-                          child: Icon(
-                            Icons.favorite_border_outlined
-                          ),
-                        ),
-                        Text(characterList[index].text,
+                                      /*Text(characterList[index].text,
                             softWrap: true,
                             textAlign: TextAlign.center,
                             style:GoogleFonts.robotoCondensed(fontSize: 50,
                                 fontStyle: FontStyle.italic,
-                                fontWeight: FontWeight.bold)),
-                        SizedBox(height: 10,),
-                        Text(characterList[index].author??"NaN"),
-                        SafeArea(
-                          child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                             horizontal: 20,)
-                          )
-                        )
-                      ],
-                    ),
-                  );
-                }),
-          ),
+                                fontWeight: FontWeight.bold)),*/
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      Text(characterList[index].author ?? "NaN",
+                                      style: GoogleFonts.roboto(fontWeight: FontWeight.w700),),
+                                      SafeArea(
+                                          child: Padding(
+                                              padding: const EdgeInsets.symmetric(
+                                                horizontal: 20,
+                                              ))),
+                                      SizedBox(height: 10,),
+                                      IconButton(
+                                          onPressed: (){
+                                            _shareFuntion(characterList[index].text);
+                                          },
+                                          icon: Icon(
+                                            Icons.share,
+                                            size: 30,  ))
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          })
+                      ) :Container()
+
+                    ],
+
+                  ),
+                ))
+          ],
         ));
   }
-  Widget  clipper(){
-    return Text(
-      ''
+
+  Widget topBar() {
+    return AppBar(
+      centerTitle: true,
+      leading: Builder(
+        builder: (BuildContext context){
+          return IconButton(
+              onPressed: ()=>{},
+              icon: Icon(Icons.menu, color:Colors.white,size: 40,));
+        },
+      ),
+      title: Text('All Quotes',style: GoogleFonts.raleway(
+          fontSize: 32,
+          color: Colors.white,
+          fontWeight: FontWeight.w800),
+          ),
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
     );
   }
 }
